1e80c3d5bdb65ed9b243c56ca00ddd674df31459
Osikvlad <Osikvlad9759@gmail.com>
2020-07-17 16:48:12 +0300

    PHP

--- index.php
+++ index.php
@@ -19,64 +19,75 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
             integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
             crossorigin="anonymous"></script>
-    <script type="text/javascript" src='js/ajax.js'></script>
+    <!--    <script type="text/javascript" src='js/ajax.js'></script>-->
 
 </head>
 <body>
 <?php
-//    function getArticles($pageNum)
-//    {
-//        if (array_key_exists('test', $_POST)) {
-//            getArticles();
-//        }
-//        $curl = curl_init();
-//        if ($pageNum == '') {
-//            $pageNum = 1;
-//        }
-//        curl_setopt_array($curl, array(
-//            CURLOPT_URL => "http://newsapi.org/v2/everything?q=Linux&language=en&sortBy=publishedAt&apiKey=6e6d9f7d53644208b1e46ea7b7a47eab&pageSize=30&page=" . $pageNum,
-//            CURLOPT_RETURNTRANSFER => true,
-//            CURLOPT_TIMEOUT => 30,
-//            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-//            CURLOPT_CUSTOMREQUEST => "GET",
-//            CURLOPT_HTTPHEADER => array(
-//                "cache-control: no-cache"
-//            ),
-//        ));
-//
-//        $response = curl_exec($curl);
-//        $err = curl_error($curl);
-//        curl_close($curl);
-//        $response = json_decode($response, true);
-//        return $response['articles'];
-//
-//    }
-//?>
+
+function getArticles($pageNum)
+{
+    if(isset($_POST['1'])){
+        $pageNum = 1;
+    }elseif(isset($_POST['2'])){
+        $pageNum = 2;
+    }elseif(isset($_POST['3'])){
+        $pageNum = 3;
+    }
+    $curl = curl_init();
+    if ($pageNum == '') {
+        $pageNum = 1;
+    }
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => "http://newsapi.org/v2/everything?q=Linux&language=en&sortBy=publishedAt&apiKey=6e6d9f7d53644208b1e46ea7b7a47eab&pageSize=30&page=" . $pageNum,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "cache-control: no-cache"
+        ),
+    ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    $response = json_decode($response, true);
+    return $response['articles'];
+}
+?>
 <div class="container text-center">
     <div class="row" id="article-box">
-    </div>
-    <div id="article" class="d-none">
-        <div class="article-wrapper col-lg-12">
-            <img class="col-lg-3 article-image" src="">
-            <div class="col-lg-9 text-justify">
-                <a href="" class="article-title mt-2" id="#article-title"></a>
-                <div class="article-author mt-2"></div>
-                <div class="article-description mt-2"></div>
-                <div class="article-created mt-2"></div>
-                <div class="article-content mt-2"></div>
-            </div>
-        </div>
+                <?php foreach (getArticles(1) as $article): ?>
+                <div class="article-wrapper col-lg-12">
+                    <img class="col-lg-3 article-image" src="<?php echo $article['urlToImage'] ? $article['urlToImage'] : 'http://placehold.it/350x250' ?>">
+                    <div class="col-lg-9 text-justify">
+                        <a href="<?php echo $article['url'] ?>" class="article-title mt-2"
+                           id="#article-title"><?php echo $article['title'] ?></a>
+                        <div class="article-author mt-2"><?php echo $article['source']['name'] ?></div>
+                        <div class="article-description mt-2"><?php echo strip_tags($article['description']) ?></div>
+                        <div class="article-created mt-2"><?php echo $article['publishedAt'] ?></div>
+                        <div class="article-content mt-2"><?php echo $article['content'] ?></div>
+                    </div>
+                </div>
+                <?php endforeach; ?>
     </div>
     <div class="pagination w-100">
-        <div class="text-center">
-            <input class="page-num" type="button" value="1"/>
-            <input class="page-num" type="button" value="2"/>
-            <input class="page-num" type="button" value="3"/>
+        <div class="text-center" style="display: flex;">
+            <form method="post" action="index.php">
+                <input class="page-num" name="1" type="submit" value="1"/>
+            </form>
+            <form method="post">
+                <input class="page-num" name="2" type="submit" value="2"/>
+            </form>
+            <form method="post">
+                <input class="page-num" name="3" type="submit" value="3"/>
+            </form>
         </div>
-
     </div>
 </div>
-</div>
+
+
 
 </body>
 </html>
